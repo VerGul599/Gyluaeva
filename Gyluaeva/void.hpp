d6d3ab50bdef2d90ddf285a48522c9165345cb5e
Вероника Гуляева <Veronikaguylaeva@yandex.ru>
2024-06-25 18:25:56 +0500

    dfgthjkl;RTGHYUJIKJUHYGTRFEDWDRFTGYHUJIKOLP;['

--- Gyluaeva/void.hpp
+++ Gyluaeva/void.hpp
@@ -1,60 +1,17 @@
-#include "void.hpp"
+#ifndef VOID_HPP
+#define VOID_HPP
 
-void manualInput(float arr[], int size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        std::cout << "Введите элемент " << i + 1 << ": ";
-        std::cin >> arr[i];
-    }
-}
+#include <iostream>
 
-void autoInput(float arr[], int size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        arr[i] = rand() % 100;
-    }
-}
+void manualInput(float arr[], int size);
 
-void printArray(float arr[], int size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        std::cout << arr[i] << " ";
-    } 
-}
+void autoInput(float arr[], int size);
 
-void bubbleSortDescending(float arr[], int size)
-{
-    for (int i = 0; i < size - 1; ++i) 
-    {
-        for (int j = 0; j < size - i - 1; ++j) 
-        {
-            if (arr[j] < arr[j + 1]) {
-                float temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    std::cout << "Массив отсортирован по убыванию";
-}
+void printArray(float arr[], int size);
 
-void bubbleSortAscending(float arr[], int size)
-{
-    for (int i = 0; i < size - 1; ++i)
-    {
-        for (int j = 0; j < size - i - 1; ++j) 
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                float temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    std::cout << "Массив отсортирован по возрастанию";
-}
+void bubbleSortDescending(float arr[], int size);
+
+void bubbleSortAscending(float arr[], int size);
+
+#endif // !VOID_HPP
 
